6a16a75db2f6e7e7ede83f99a086e71f5536a133
ymougenel <yann.mougenel@gmail.com>
2020-05-20 23:34:16 +0200

    init project

---
+++ src/main/kotlin/com/ymougenel/bgaranking/BgaRankingApplication.kt
@@ -0,0 +1,11 @@
+package com.ymougenel.bgaranking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BgaRankingApplication
+
+fun main(args: Array<String>) {
+	runApplication<BgaRankingApplication>(*args)
+}
---
+++ src/test/kotlin/com/ymougenel/bgaranking/BgaRankingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ymougenel.bgaranking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BgaRankingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
